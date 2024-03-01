768c3f3188072508ffd0b72075d698ee9a75fbc5
Jan Hamal Dvořák <mordae@anilinux.org>
2024-03-01 14:44:46 +0100

    Use yet another bias pattern
    This one works equally well for both string and weak signals.

--- src/main.c
+++ src/main.c
@@ -103,7 +103,7 @@ static void bias_init(int in_pin, int out_pin)
 
 	const uint16_t insn[] = {
 		pio_encode_mov_not(pio_pins, pio_pins) | pio_encode_sideset(1, 1),
-		pio_encode_nop() | pio_encode_sideset(1, 0) | pio_encode_delay(15),
+		pio_encode_mov_not(pio_pins, pio_pins) | pio_encode_sideset(1, 1),
 		pio_encode_nop() | pio_encode_sideset(1, 0) | pio_encode_delay(15),
 		pio_encode_nop() | pio_encode_sideset(1, 0) | pio_encode_delay(15),
 	};
